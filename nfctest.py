486ef400ead56c35bae1df1ccc5eeced4dea22f8
flexshure <2fjdube@gmail.com>
2024-04-27 19:45:08 -0700

    simplified

--- nfctest.py
+++ nfctest.py
@@ -1,19 +1,15 @@
-import RPi.GPIO as GPIO
-import MFRC522
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
 
-reader = MFRC522.MFRC522()
+reader = SimpleMFRC522()
 
-def read_data(sector):
-    status = reader.MFRC522_Auth(reader.PICC_AUTHENT1A, sector, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], reader.uid)
-    if status == reader.MI_OK:
-        data = reader.MFRC522_Read(sector)
-        return data
-    else:
-        return None
-
-sector_to_read = 1
-read_data = read_data(sector_to_read)
-if read_data is not None:
-    print("Data read:", read_data)
-else:
-    print("Error reading data")
+try:
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id,text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise

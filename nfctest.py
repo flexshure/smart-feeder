7b4c786c8b760cf12dbf4f43decc2b2581817d20
flexshure <2fjdube@gmail.com>
2024-04-27 19:31:04 -0700

    simple nfc read test

--- nfctest.py
+++ nfctest.py
@@ -1,3 +1,19 @@
-import nfc
+import RPi.GPIO as GPIO
+import MFRC522
 
-clf = nfc.ContactlessFrontend('usb')
+reader = MFRC522.MFRC522()
+
+def read_data(sector):
+    status = reader.MFRC522_Auth(reader.PICC_AUTHENT1A, sector, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], reader.uid)
+    if status == reader.MI_OK:
+    data = reader.MFRC522_Read(sector)
+    return data
+    else:
+    return None
+
+sector_to_read = 1
+read_data = read_data(sector_to_read)
+if read_data is not None:
+print(“Data read:”, read_data)
+else:
+print(“Error reading data”)

6da7c9ae4689a7c5d65992a700ba82c9348a3fce
Nicolas Conrad <nicolasconrad@Nicolass-MacBook-Air.local>
2024-04-27 18:09:07 -0700

    added Kintone data parsing

--- requestKintoneEx.py
+++ requestKintoneEx.py
@@ -1,5 +1,23 @@
 import requests
-response = requests.get('https://nfc-smart-feeder.kintone.com/k/v1/record.json?app=1&id=1', headers={'X-Cybozu-API-Token': 'CvLah6cYe2KZgVxbKeAgjVERCcnMSDbXjd3Hrab5'})
+import json
 
-print(response.json)
-print(response.text)
+id = 1
+data = {}
+while True:
+    url = 'https://nfc-smart-feeder.kintone.com/k/v1/record.json?app=1&id=' + str(id)
+    response = requests.get(url, headers={'X-Cybozu-API-Token': 'CvLah6cYe2KZgVxbKeAgjVERCcnMSDbXjd3Hrab5'})
+    if "is not found" in response.text:
+        break
+   
+    response = json.loads(response.text)
+
+    #parse response
+    name = response["record"]["Text"]["value"]
+    timeBtwnMeals = response["record"]["Number_0"]["value"]
+    quantityFood = response["record"]["Number"]["value"]
+   
+    #store data in dictionary
+    data[name] = {"UnitsFood": quantityFood, "TimeBtwnMeals": timeBtwnMeals}
+    id = id + 1
+
+print(data)

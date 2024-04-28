ed3121997cb72bb53215041afc1029c58bc635a7
Nicolas Conrad <nicolasconrad@Nicolass-MacBook-Air.local>
2024-04-28 01:59:03 -0700

    changed petID to snake case:

--- Kintone.py
+++ Kintone.py
@@ -26,7 +26,7 @@ class Kintone:
             self.schedule_table[pet_id] = {"name": name, "units_food": quantity_food, "meal_times": meal_times, "NFC_ID": nfc_tag, "food_type": food_type}
             id = id + 1
 
-    def get_last_eaten_timestamp(self, petID):
+    def get_last_eaten_timestamp(self, pet_id):
         id = 1
         while True:
             url = "https://nfc-smart-feeder.kintone.com/k/v1/record.json?app=2&id=" + str(id)
@@ -35,7 +35,7 @@ class Kintone:
             if "is not found" in response.text:
                 break
             response = json.loads(response.text)
-            if petID == response["record"]["$id"]["value"]:
+            if pet_id == response["record"]["$id"]["value"]:
                 #convert time representations
                 return response["record"]["Text_1"]["value"]  
             id = id + 1
@@ -50,7 +50,7 @@ class Kintone:
 
     def add_empty_entry_last_eaten_app(self, pet_ID, pet_name):
         api_url = "https://nfc-smart-feeder.kintone.com/k/v1/record.json"
-        #send a new data record, ISSUE: Can't set Record_number (aka petID)
+        #send a new data record, ISSUE: Can't set Record_number (aka pet_id)
         postJson = {'app': 2,'record': {'Text': {'value': pet_name},'Text_1': {'value': 'NULL'}}}
         post = requests.post(api_url, headers={'X-Cybozu-API-Token': 'RKylBI2WhrLWJoSba87HT3b5QgBuuWIh6xF1Plyc'}, json=postJson)
 
@@ -63,12 +63,12 @@ class Kintone:
                 return entry
 
     def push_updated_NFC_to_db(self, pet_id, NFC_ID):
-        url = "https://nfc-smart-feeder.kintone.com/k/v1/record.json?app=1&id=" + str(petID)
+        url = "https://nfc-smart-feeder.kintone.com/k/v1/record.json?app=1&id=" + str(pet_id)
         postJson = {'app': 1,'id': pet_id,'record': {'Text_1': {'value': NFC_ID}}}
         post = requests.put(url, headers={'X-Cybozu-API-Token': 'CvLah6cYe2KZgVxbKeAgjVERCcnMSDbXjd3Hrab5'}, json=postJson)
     
     def push_last_eaten_timestamp(self, pet_id, now):
-        url = "https://nfc-smart-feeder.kintone.com/k/v1/record.json?app=2&id=" + str(petID)
+        url = "https://nfc-smart-feeder.kintone.com/k/v1/record.json?app=2&id=" + str(pet_id)
         postJson = {'app': 2,'id': pet_id,'record': {'Text_1': {'value': now}}}
         post = requests.put(url, headers={'X-Cybozu-API-Token': 'RKylBI2WhrLWJoSba87HT3b5QgBuuWIh6xF1Plyc'}, json=postJson)
     
@@ -80,18 +80,4 @@ class Kintone:
             my_time = Time(hours=int(split[0]), minutes=int(split[1]))
             meal_times_formatted.append(my_time)
         return meal_times_formatted
-
-    '''
-    def updateLastTimeEaten(petName, lastTimeEaten):
-        for i in range(0, len(petData)):
-            
-            api_url = "https://nfc-smart-feeder.kintone.com/k/v1/record.json?app=2&id=1"
-            #send a new data record
-            postJson = {'app': 2,'record': {'Text': {'value': petName},'Text_1': {'value': lastTimeEaten}}}
-            post = requests.post(api_url, headers={'X-Cybozu-API-Token': 'RKylBI2WhrLWJoSba87HT3b5QgBuuWIh6xF1Plyc'}, json=postJson)
-
-        #update a data record
-        postJson = {'app': 2,'id': 1,'record': {'Text': {'value': 'YEEE'},'Text_1': {'value': 'April 27, 6:52 PM'}}}
-        post = requests.put(api_url, headers={'X-Cybozu-API-Token': 'RKylBI2WhrLWJoSba87HT3b5QgBuuWIh6xF1Plyc'}, json=postJson)
-    '''
     

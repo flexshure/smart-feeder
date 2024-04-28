89cfd536d8463b96193afb79d02cf79b1510d78e
Nicolas Conrad <nicolasconrad@Nicolass-MacBook-Air.local>
2024-04-27 20:00:02 -0700

    updated get request to kimtone. Added notes on main loop

--- main.py
+++ main.py
@@ -3,7 +3,23 @@ from gpiozero import LED
 
 
 def main():
-    pass 
+    while True:
+        '''
+        Main loop structure:
+
+        check NFC sensor
+            --> if tag detected, refresh data from kintone
+                match NFC tag detected to a pet
+                
+                check if pet is able to eat (check set times for pet's meals and last time pet has eaten)
+                    If able to eat --> dispense food (use given pet's units of food to rotate motor a certain number of degrees)
+                    If not able to eat --> (maybe play sound)
+
+            
+            Potential ideas / stretch goals : 
+                - If pet hasn't eaten 10 minutes past scheduled meal, play a sound or song
+        '''
+    pass
 
 if __name__ == "__main__":
     main()
--- requestKintoneEx.py
+++ requestKintoneEx.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+#Get and parse all data from Kintone about individual pet's needs
 id = 1
 data = {}
 while True:
@@ -8,16 +9,19 @@ while True:
     response = requests.get(url, headers={'X-Cybozu-API-Token': 'CvLah6cYe2KZgVxbKeAgjVERCcnMSDbXjd3Hrab5'})
     if "is not found" in response.text:
         break
-   
+    
     response = json.loads(response.text)
 
     #parse response
     name = response["record"]["Text"]["value"]
-    timeBtwnMeals = response["record"]["Number_0"]["value"]
+    mealTimes = response["record"]["Text_0"]["value"]
+    mealTimes = mealTimes.split(',')
     quantityFood = response["record"]["Number"]["value"]
-   
+    nfcTag = response["record"]["Text_1"]["value"]
+    foodType = response["record"]["Text_2"]["value"]
     #store data in dictionary
-    data[name] = {"UnitsFood": quantityFood, "TimeBtwnMeals": timeBtwnMeals}
+    data[name] = {"UnitsFood": quantityFood, "MealTimes": mealTimes, "NFC Tag": nfcTag, "FoodType": foodType}
     id = id + 1
+
 
 print(data)
